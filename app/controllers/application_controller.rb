de8b59dc06967e40dc16addca32235ae39a2e75b
rtmalone <rtylermalone@gmail.com>
2014-06-16 12:23:44 -0500

    implementation of omniauth twitter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :authenticate_user!
+
+  def ensure_signup_complete
+    return if action_name == 'finish_signup'
+
+    if current_user && !current_user.email_verified?
+      redirect_to finish_signup_path(current_user)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: 'home#index'
+
+  match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
