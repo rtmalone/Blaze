9a31aa2bb09276cdd62cbf2d090554b7312bae39
rtmalone <rtylermalone@gmail.com>
2014-06-24 15:37:28 -0500

    api integrated and saves to db

--- app/models/trail_api.rb
+++ app/models/trail_api.rb
@@ -2,12 +2,13 @@ class TrailApi
   include HTTParty
   base_uri 'https://outdoor-data-api.herokuapp.com'
   default_params api_key: ENV["TRAIL_API_KEY"]
+  attr_accessor :name, :length, :city, :state
 
   def initialize
   end
 
-  def for_state(state)
-    @options = {query: { q:{activities_activity_type_name_cont: "hiking", state_eq: state} }}
-    self.class.get("/api.json", @options)
+  def self.for_state
+    @options = {query: { q:{activities_activity_type_name_cont: "hiking", state_eq: "Tennessee"} }}
+    self.get("/api.json", @options)
   end
 end
