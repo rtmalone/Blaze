f535c957dfb6da5175788fbcee6c99169cd95e5c
rtmalone <rtylermalone@gmail.com>
2014-06-26 15:33:13 -0500

    deleted public assets folder

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Blaze
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.precompile += ['application.css.scss']
+    config.assets.precompile += ['vendor/modernizr']
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
